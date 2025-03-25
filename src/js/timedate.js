c96f2dc4ad4a9fc67a831ba59253fb041c6c9756
Hironobu Nagaya <hironobu.nagaya@gmail.com>
2025-03-25 09:23:30 +0900

    fix javascript

--- src/js/timedate.js
+++ src/js/timedate.js
@@ -1,54 +1,14 @@
 'use strict';
 
+const now = new Date();
+
 $(document).ready(function() {
-    const now = new Date();
-
-    // date-suffix
-    const year = now.getFullYear().toString();
-    const month = (now.getMonth() + 1).toString();
-    const date = now.getDate().toString();
-    $('#date-suffix').replaceWith(
-        year
-        + month.padStart(2, '0')
-        + date.padStart(2, '0'));
-
-    // today
-    $('#today').replaceWith(
-        year
-        + '年'
-        + month
-        + '月'
-        + date
-        + '日');
-
-    // date
-    const weekday = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(now);
-    const monthName = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(now);
-    const hours = (now.getHours()).toString().padStart(2, '0');
-    const minutes = (now.getMinutes()).toString().padStart(2, '0');
-    const seconds = (now.getSeconds()).toString().padStart(2, '0');
-    const timezone = 'JST';
-    $('.date').text(
-        weekday
-        + ' '
-        + monthName
-        + ' '
-        + date
-        + ' '
-        + hours
-        + ':'
-        + minutes
-        + ':'
-        + seconds
-        + ' '
-        + timezone
-        + ' '
-        + year);
-
-    // calendar
     const header = 'Su Mo Tu We Th Fr Sa';
-    const longMonthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(now);
-    const bareTitle = longMonthName + ' ' + year;
+    const monthName = new Intl.DateTimeFormat('en-US', {
+        month: 'long'
+    }).format(now);
+    const year = now.getFullYear().toString();
+    const bareTitle = monthName + ' ' + year;
     const lengthDiff = header.length - bareTitle.length;
     const title = ' '.repeat(lengthDiff / 2 + (lengthDiff & 1))
         + bareTitle
@@ -58,25 +18,22 @@ $(document).ready(function() {
     const endDate = new Date();
     endDate.setMonth(endDate.getMonth() + 1);
     endDate.setDate(0);
-    const startDay =  1 - startDate.getDay() % 7;
-    const endDay = endDate.getDate();
-    const currentDay = now.getDate();
-    var calendar = title
-        + '<br>'
-        + header
-        + '<br>';
-    for (var i = startDay; i <= endDay; i += 7) {
+    const start =  1 - startDate.getDay() % 7;
+    const end = endDate.getDate();
+    const current = now.getDate();
+    var calendar = title + '<br>' + header + '<br>';
+    for (var i = start; i <= end; i += 7) {
         for (var j = 0; j < 7; j++) {
-            const targetDay = i + j;
-            if (targetDay < 1 || targetDay > endDay) {
+            const target = i + j;
+            if (target < 1 || target > end) {
                 calendar += '  ';
             } else {
-                if (targetDay == currentDay) {
+                if (target == current) {
                     calendar += '<span class="revert">'
-                        + targetDay.toString().padStart(2, ' ')
+                        + target.toString().padStart(2, ' ')
                         + '</span>';
                 } else {
-                    calendar += targetDay.toString().padStart(2, ' ');
+                    calendar += target.toString().padStart(2, ' ');
                 }
             }
             if (j < 6) {
@@ -86,5 +43,40 @@ $(document).ready(function() {
         calendar += '<br>';
     }
     $('.calendar').html(calendar);
+});
+
+$(document).ready(function() {
+    const year = now.getFullYear().toString();
+    const weekday = new Intl.DateTimeFormat('en-US', {
+        weekday: 'short'
+    }).format(now);
+    const monthName = new Intl.DateTimeFormat('en-US', {
+        month: 'short'
+    }).format(now);
+    const date = now.getDate().toString();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timezone = 'JST';
+    const dateOutput = weekday + ' ' + monthName + ' ' + date
+        + ' ' + hours + ':' + minutes + ':' + seconds
+        + ' ' + timezone + ' ' + year;
+    $('.date').text(dateOutput);
+});
+
+$(document).ready(function() {
+    const year = now.getFullYear().toString();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const date = now.getDate().toString().padStart(2, '0');
+    const dateSuffix = year + month + date;
+    $('#date-suffix').replaceWith(dateSuffix);
+});
+
+$(document).ready(function() {
+    const year = now.getFullYear().toString();
+    const month = (now.getMonth() + 1).toString();
+    const date = now.getDate().toString();
+    const today = year + '年' + month + '月' + date + '日';
+    $('#today').replaceWith(today);
 });
 
